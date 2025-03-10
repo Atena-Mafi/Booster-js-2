db9927f62ceb612f06e1cafb44452f5d2b1993e9
Atena Mafi <atenamafi20@gmail.com>
2025-03-10 23:31:40 +0330

    Add files via upload

---
+++ js-2-1.js
@@ -0,0 +1,37 @@
+/**
+ * /*
+ *   Array properties
+ *   ----------------
+ *   Complete the function to test if an array is empty (has no values in it)
+ *
+ * @format
+ */
+
+function isEmpty(arr) {
+
+  if(arr.length===0){
+
+     return true;
+  }else{
+
+    return false;
+  }
+  // complete this statement
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+var numbers = [1, 2, 3];
+var names = [];
+
+console.log(isEmpty(numbers));
+console.log(isEmpty(names));
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  false
+  true
+*/
+
